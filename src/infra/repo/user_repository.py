4d6a22b462005430670bdc651ba92309e388155c
Programador Lhama <programadorlhama@gmail.com>
2021-01-03 15:16:05 -0300

    feat: Implementing models in domain layer

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -1,8 +1,8 @@
 # pylint: disable=E1101
 
-from collections import namedtuple
+from src.domain.models import Users
 from src.infra.config import DBConnectionHandler
-from src.infra.entities import Users
+from src.infra.entities import Users as UsersModel
 
 
 class UserRepository:
@@ -16,15 +16,13 @@ class UserRepository:
         :return - tuple with new user inserted
         """
 
-        InsertData = namedtuple("Users", "id name, password")
-
         with DBConnectionHandler() as db_connection:
             try:
-                new_user = Users(name=name, password=password)
+                new_user = UsersModel(name=name, password=password)
                 db_connection.session.add(new_user)
                 db_connection.session.commit()
 
-                return InsertData(
+                return Users(
                     id=new_user.id, name=new_user.name, password=new_user.password
                 )
             except:
